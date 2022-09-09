ed2595d065bc7a9b5ef78a123f137027f7830274
Sagar Patel <sagar1.citrusbug@gmail.com>
2022-09-09 09:27:42 +0530

    initial commit

---
+++ Custom/models.py
@@ -0,0 +1,56 @@
+import imp
+from django.db import models
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager 
+
+
+class userManager(BaseUserManager):
+    def create_user(self, name, email, password = None):
+        if not name:
+            raise ValueError("Please Enter Name")
+        if not email:
+            raise ValueError("Email is required")
+        user  = self.model(
+            name = name,
+            email = self.normalize_email(email)
+        )
+        user.set_password(password)
+
+        user.save(using = self._db)
+        
+        return user
+    def create_superuser(self, name, email,password = None):
+        user = self.create_user(
+            name = name,
+             email = self.normalize_email(email),
+              password= password
+            )
+        user.is_admin = True
+        user.is_superuser = True
+        user.save(using  = self._db)
+        return user
+        
+
+
+# Create your models here.
+class User(AbstractBaseUser):
+
+    name = models.CharField(max_length=50, verbose_name=" User Name: ")
+    email = models.EmailField(max_length=50,unique=True, verbose_name="E-mail Address :")
+    is_active =  models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin=models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default = False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['name']
+    objects  = userManager()
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj = None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+    
